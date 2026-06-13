41aa6080c10c7d9da67597cd3d0066a8859f6a3d
Nimish Chauhan <2468nimishchauhan@gmail.com>
2026-06-14 01:19:59 +0530

    first commit

---
+++ day2/index.js
@@ -0,0 +1,40 @@
+// let name = "nimish" ;
+// let age = 20;
+// age = 23;
+// console.log(name , age);
+ 
+// const acc = 54;
+// acc= 56;
+// console.log(acc);
+
+if(true){
+    var c= 5;
+}
+console.log(c);
+
+// -------------data types-----------
+
+// primitive -- num , string , boolean , undef , bigint , symbol
+//number
+let d = 45;
+let o = 4.556;
+console.log(d , o);
+
+//string
+let r = "mercedes";
+let h = "kk";
+console.log(r,h);
+
+//boolean
+let login = "true";
+let j = "false";
+
+//undefined
+
+let user;
+const p  = 5;  //isko khali nhi chod sakte
+console.log(user , p);
+
+//bigint
+
+
