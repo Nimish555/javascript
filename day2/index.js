cee9cda6c4e214691c849ecf843cccd2f0b3d58a
Nimish Chauhan <2468nimishchauhan@gmail.com>
2026-06-22 21:35:08 +0530

    od

--- day2/index.js
+++ day2/index.js
@@ -36,20 +36,21 @@ const p  = 5;  //isko khali nhi chod sakte
 console.log(user , p);
 
 //bigint------
-let num = 456895698556n;
-console.log(num);
+// let num = 456895698556n;
+// console.log(num);
 
 //symbol-----------
-let id1 = Symbol("id");
-let id2 = Symbol("id");
+// let id1 = Symbol("id");
+// let id2 = Symbol("id");
 
-console.log(id1);
-console.log(id2);
-console.log(id1 == id2);
+// console.log(id1);
+// console.log(id2);
+// console.log(id1 == id2);
 
 //Array------------
-let arr = [10 , "kim" , true , 45 , 6];
-console.log(arr);
+// let arr = [10 , "kim" , true , 45 , 6];
+// console.log(arr);
+// console.log(typeof arr);
 
 // let user1 = {
 //     name : "nimish",
@@ -57,8 +58,33 @@ console.log(arr);
 //     gender : 'male'
 // }
 
- let s = function add(){
-    console.log("hello");
+//  let s = function add(){
+//     console.log("hello");
+// }
+
+// console.log(s());
+// console.log(typeof s);
+
+
+// let a = 56;
+// a=20;
+// console.log(a);
+// let str = "erm";
+// str = "happy";
+// console.log(str);
+
+// console.log(str[2]);
+
+//non primitive datatype are mutable
+let arr = [4 , 5 , 6 , 9];
+arr.push(45);
+arr[1] = 69;
+console.log(arr);
+
+let obj = {
+    name : "Mohan",
+    age  : 54
 }
 
-console.log(s());
+obj.name = "rohan";
+console.log(obj.name);
