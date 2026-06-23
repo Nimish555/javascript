040190aced4beab99f9387c76c2b8369c3315d88
Nimish Chauhan <2468nimishchauhan@gmail.com>
2026-06-23 15:14:12 +0530

    add

--- day4/index.js
+++ day4/index.js
@@ -47,3 +47,48 @@ console.log(null<0);
 
 console.log("Rohit" > "mohit");
 
+//agar diffrenrt type hai to to pehle number mein cionvert honge , fir comaprison hoga
+console.log(null>"");
+
+
+//loop
+for(let i = 0 ; i<5 ; i++){
+    console.log(i);
+}
+
+//do while
+// let i = 0;
+// do{
+//     console.log(i);
+//     i++;
+// } while(i<10);
+
+// let a = "mohit";
+// let b = "rohit";
+// let c = a&&b;
+// let d = a||b;
+// console.log(c);
+// console.log(d);
+
+// let a = 0;
+// let b = 20;
+// console.log(a&&b);
+//if first is false then it will give first val
+
+
+// let a = 4;
+// let b = 4.36945;
+// console.log(b.toFixed(1));
+
+// console.log(b.toPrecision(2));
+// console.log(b.toString);
+
+// let a = new Number(20);
+// let b = 20;
+// console.log(typeof b);
+// console.log(typeof a);
+
+let a = new Number(23);
+let b = new Number(23);
+console.log(a==b);
+console.log(a.valueOf() == b.valueOf());
