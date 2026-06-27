91d3ec0072b44536384a049952392fe89e770dd1
Nimish Chauhan <2468nimishchauhan@gmail.com>
2026-06-27 17:57:00 +0530

    add

---
+++ day4/Array.js
@@ -0,0 +1,26 @@
+let arr = ["aman" , "kim" , "lalit"];
+let arr1 = [56 , 69 , 87 , 65];
+
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr);
+console.log(typeof arr);
+console.log(arr.push("rahul"));
+// console.log(arr.pop());
+console.log(arr.unshift("pop"));
+console.log(arr.concat(arr1));
+console.log(arr.slice(0,2));
+console.log(arr.reverse());
+
+//splice--------
+
+console.log(arr1.splice(2,3));
+console.log(arr1);
+console.log(arr);
+
+
+console.log(arr.length);
+
+let std = [
+
+]
