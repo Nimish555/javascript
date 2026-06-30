b1f5a903f8e05c30ddc0b47e623eee8a7fff5018
Nimish Chauhan <2468nimishchauhan@gmail.com>
2026-06-30 23:38:13 +0530

    aadd

---
+++ day4/Function.js
@@ -0,0 +1,58 @@
+function myFunction(){
+    console.log("cannada");
+    console.log("fill");
+};
+
+myFunction();
+
+
+
+
+function call(msg){    //parameter
+    console.log(msg);
+
+}
+
+call("I love java"); //argument
+
+
+//sum function
+function sum(a,b){
+let s = a+b;   //local variable
+console.log("sum");
+return s;
+}
+
+let v = sum(4,5);
+console.log(v);
+
+//let   vs   var
+
+function Diff(){
+    if(true){
+        let v = 10;
+        var c = 23;
+        console.log(v);
+    }
+
+    console.log(c);
+    console.log(v);
+}
+
+Diff();
+
+
+//cons fumction
+
+const arrowSum = (a,b) => {
+    console.log(a+b);
+};
+
+//multiplication
+function multi(a,b){
+    return a*b;
+};
+
+const arrowMul = (a,b) => {
+    console.log(a*b);
+};
