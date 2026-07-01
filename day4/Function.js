ad5f2d9646a363955e654cb6a038481a84851cac
Nimish Chauhan <2468nimishchauhan@gmail.com>
2026-07-01 22:30:15 +0530

    add

--- day4/Function.js
+++ day4/Function.js
@@ -1,4 +1,4 @@
-function myFunction(){
+function myFunction() {
     console.log("cannada");
     console.log("fill");
 };
@@ -8,7 +8,7 @@ myFunction();
 
 
 
-function call(msg){    //parameter
+function call(msg) {    //parameter
     console.log(msg);
 
 }
@@ -17,19 +17,19 @@ call("I love java"); //argument
 
 
 //sum function
-function sum(a,b){
-let s = a+b;   //local variable
-console.log("sum");
-return s;
+function sum(a, b) {
+    let s = a + b;   //local variable
+    console.log("sum");
+    return s;
 }
 
-let v = sum(4,5);
+let v = sum(4, 5);
 console.log(v);
 
 //let   vs   var
 
-function Diff(){
-    if(true){
+function Diff() {
+    if (true) {
         let v = 10;
         var c = 23;
         console.log(v);
@@ -44,37 +44,46 @@ Diff();
 
 //cons fumction
 
-const arrowSum = (a,b) => {
-    console.log(a+b);
+const arrowSum = (a, b) => {
+    console.log(a + b);
 };
 
 //multiplication
-function multi(a,b){
-    return a*b;
+function multi(a, b) {
+    return a * b;
 };
 
-const arrowMul = (a,b) => {
-    console.log(a*b);
+const arrowMul = (a, b) => {
+    console.log(a * b);
 };
 
 
 
-let f =  [44 , 58 , 98 , 78 , 56];
+let f = [44, 58, 98, 78, 56];
 f.forEach((val) => {
     console.log(val);
 }
 )
-    
 
-let b = ["pune" , "agra" , "delhi" , "mumbai"];
-b.forEach((val , idx) => {
-    console.log(val.toUpperCase() , idx);
+
+let b = ["pune", "agra", "delhi", "mumbai"];
+b.forEach((val, idx, b) => {
+    console.log(val.toUpperCase(), idx, b);
 }
 
-) ;
+);
+
+// sq of number
+
+let n = [1 , 2 , 3 , 5];
+n.forEach((val) => {
+      console.log(val*val);
+}
+);
 
 
- 
+
+
 
 
 
